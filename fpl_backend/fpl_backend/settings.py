59cc02c1485f6f922253e4a7b9590b01fa3a8a1f
Pujan Pandey <arghakhanchipujan@gmail.com>
2026-08-17 22:27:27 +0545

    made changes in frontend to make it device compatible

--- fpl_backend/fpl_backend/settings.py
+++ fpl_backend/fpl_backend/settings.py
@@ -122,8 +122,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day',
+        'anon': '10000/day',
+        'user': '50000/day',
     }
 }
 
