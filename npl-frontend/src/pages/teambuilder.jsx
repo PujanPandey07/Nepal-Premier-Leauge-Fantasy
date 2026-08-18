8bb2323b5bda7b379a2340fc71bf478be988b7ff
Pujan Pandey <arghakhanchipujan@gmail.com>
2026-08-18 18:29:11 +0545

    updated team building logic w.r.t backend data

--- npl-frontend/src/pages/teambuilder.jsx
+++ npl-frontend/src/pages/teambuilder.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { TeamContext, ROLE_LIMITS, getFantasyRole } from '../context/TeamContext'
+import { TeamContext, ROLE_LIMITS, getFantasyRole } from "../context/TeamContext.jsx";
 import Navbar from '../components/navbar'
 import PlayerDrawer from '../components/PlayerDrawer'
 import PlayerDetailModal from '../components/PlayerDetailModal'
